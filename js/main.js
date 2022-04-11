8ce6b1409c6939fbeedad428bd58317aa3b07da5
Ewelina <ewelinazawol@gmail.com>
2022-04-11 12:24:37 +0100

    Add Animated Mobile Menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 const btnMenu = document.querySelector('#btnMenu');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnMenu.addEventListener('click', function(){ 
     console.log('clik open menu');
 
     if (header.classList.contains('open')) { // Close btnMenu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
     } 
     else { // Open btn Menu
+        body.classList.add('noscroll');
         header.classList.add('open'); 
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
     }
 })
  
